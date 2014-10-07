a0b0c7203a80b4edda600604cc94c31bb81bc78d
Dave Wallin <dwallin@archer-group.com>
2014-10-07 11:37:26 -0400

    initial commit. Lacking tests and docs

---
+++ README.md
@@ -0,0 +1,89 @@
+# grunt-harp-post
+
+> Task that interactively creates blog posts or other content for HARP
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-harp-post --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-harp-post');
+```
+
+## The "harp_post" task
+
+### Overview
+In your project's Gruntfile, add a section named `harp_post` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  harp_post: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+});
+```
+
+### Options
+
+#### options.separator
+Type: `String`
+Default value: `',  '`
+
+A string value that is used to do something with whatever.
+
+#### options.punctuation
+Type: `String`
+Default value: `'.'`
+
+A string value that is used to do something else with whatever else.
+
+### Usage Examples
+
+#### Default Options
+In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
+
+```js
+grunt.initConfig({
+  harp_post: {
+    options: {},
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+});
+```
+
+#### Custom Options
+In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
+
+```js
+grunt.initConfig({
+  harp_post: {
+    options: {
+      separator: ': ',
+      punctuation: ' !!!',
+    },
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
+    },
+  },
+});
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/harp_post.js
@@ -0,0 +1,164 @@
+/*
+ * grunt-harp-post
+ * https://github.com/dwarcher/grunt-harp-post
+ *
+ * Copyright (c) 2014 Dave Wallin
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('harp_post', 'Task that interactively creates blog posts or other content for HARP', function() {
+    
+    var fs = require('fs');
+    var moment = require('moment');
+    var inquirer = require('inquirer');
+    var exec = require('child_process').exec;
+    var jf = require('jsonfile');
+    var mkdirp = require('mkdirp');
+
+    var curTime = moment();
+
+
+    function slugify (sz) {
+      return sz.toLowerCase().replace(/[^a-z0-9]/g, '-');
+    }
+
+    function copyFile(source, target, cb) {
+      var cbCalled = false;
+
+      var rd = fs.createReadStream(source);
+      rd.on("error", function(err) {
+        done(err);
+      });
+      var wr = fs.createWriteStream(target);
+      wr.on("error", function(err) {
+        done(err);
+      });
+      wr.on("close", function(ex) {
+        done();
+      });
+      rd.pipe(wr);
+
+      function done(err) {
+        if (!cbCalled) {
+          cb(err);
+          cbCalled = true;
+        }
+      }
+    }
+
+
+    var baseContentType = {
+      path: "posts/",
+      templatePath: "templates/post.md",
+      questions: [ 
+        { 
+          type: "input", 
+          message: "Title",
+          name: "title",
+          default: "untitled" 
+        },
+        { 
+          type: "date", 
+          message: "Date (MM-DD-YYYY)",
+          name: "date",
+          default: curTime.format("MM-DD-YYYY")
+        }
+      ]
+    };
+
+  // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      openPage: false,
+      editPage: false,
+      destFolderBase: "public/"
+    });
+
+    if(options.path)
+    {
+      baseContentType.path = options.path;
+    }
+
+    if(options.templatePath)
+    {
+      baseContentType.templatePath = options.templatePath;
+    }
+
+    if(options.fields)
+    {
+      // add to base questions
+      baseContentType.questions = baseContentType.questions.concat(options.fields);
+    }
+
+
+    var contentType = baseContentType;
+
+    var contentDataFile = options.destFolderBase + contentType.path + "_data.json";
+
+    var blogEntries;
+
+    if (fs.existsSync(contentDataFile)) {
+      blogEntries = jf.readFileSync(contentDataFile);
+    } else {
+      blogEntries = { };
+    }
+
+    var done = this.async();
+    inquirer.prompt( contentType.questions, function(answers) {
+        var pickedDate = moment(answers.date, "MM-DD-YYYY");
+        var sluggedTitle = slugify(answers.title);
+        var fileName = pickedDate.format("YYYY-MM-DD") + "-" + sluggedTitle;
+
+        // add extra date info
+        answers.date = pickedDate.format();
+        answers.month = pickedDate.format("MM");
+        answers.day = pickedDate.format("DD");
+        answers.year =pickedDate.format("YYYY");
+
+        // update blog entries..
+        blogEntries[fileName] = answers;
+
+        // write updated data file
+
+        // Create posts folder if it doesn't exist
+        mkdirp.sync(options.destFolderBase + contentType.path);
+
+        jf.writeFileSync(contentDataFile, blogEntries);
+
+        var destPath = options.destFolderBase + contentType.path + fileName + ".md";
+        copyFile(contentType.templatePath, destPath, function() {
+        console.log("Created " + destPath + "\n");
+
+          // FIXME: make these optional
+       
+       if(options.editPage)
+       {
+          // attempt to edit the page.. Note that this probably only works on macs.
+
+          exec('open ' + destPath, function (error, stdout, stderr) {
+            // output is in stdout
+          });        
+       }
+
+        if(options.openPage)
+        {
+          // attempt to open the page.. Note that this probably only works on macs.
+          exec('open http://localhost:9000/' + contentType.path + fileName + ".html", function (error, stdout, stderr) {
+            // output is in stdout
+          });
+        }
+
+
+        done();
+
+      });
+    } );
+  });
+
+};
